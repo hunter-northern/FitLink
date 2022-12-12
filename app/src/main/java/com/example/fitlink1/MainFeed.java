09d6f163cc6856593b5e279cc8269b5a1f65af42
Hunter <northern@iastate.edu>
2022-12-12 15:58:57 -0600

    Profile Main Feed Semi Working

--- app/src/main/java/com/example/fitlink1/MainFeed.java
+++ app/src/main/java/com/example/fitlink1/MainFeed.java
@@ -2,11 +2,29 @@ package com.example.fitlink1;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -14,15 +32,17 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class MainFeed extends Fragment {
-
+    RecyclerView recycler;
+    ArrayList<Posts> postList = new ArrayList<>();
+    PostAdapter pAdapter;
+    FirebaseFirestore fsPosts;
+    ListView postListView;
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private static final String username = "username";
 
     // TODO: Rename and change types of parameters
     private String mParam1;
-    private String mParam2;
 
     public MainFeed() {
         // Required empty public constructor
@@ -33,15 +53,13 @@ public class MainFeed extends Fragment {
      * this fragment using the provided parameters.
      *
      * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
      * @return A new instance of fragment MainFeed.
      */
     // TODO: Rename and change types and number of parameters
-    public static MainFeed newInstance(String param1, String param2) {
+    public static MainFeed newInstance(String param1) {
         MainFeed fragment = new MainFeed();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
+        args.putString(username, param1);
         fragment.setArguments(args);
         return fragment;
     }
@@ -50,8 +68,7 @@ public class MainFeed extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+            mParam1 = getArguments().getString(username);
         }
     }
 
@@ -59,6 +76,46 @@ public class MainFeed extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main_feed, container, false);
+
+        final View root = inflater.inflate(R.layout.fragment_main_feed, container, false);
+        //recycler = root.findViewById(R.id.recycleview);
+        fsPosts = FirebaseFirestore.getInstance();
+
+//        recycler.setAdapter(pAdapter);
+//        recycler.setLayoutManager(new LinearLayoutManager(getContext()));
+        postListView = root.findViewById(R.id.listView);
+
+
+        fsPosts.collection("posts").get().addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+            @Override
+            public void onSuccess(@NonNull QuerySnapshot queryDocumentSnapshots) {
+                Toast.makeText(getContext(), "Req Successful", Toast.LENGTH_SHORT).show();
+                if (!queryDocumentSnapshots.isEmpty()) {
+                    Toast.makeText(getContext(), "Check 2", Toast.LENGTH_SHORT).show();
+                    List<DocumentSnapshot> list = queryDocumentSnapshots.getDocuments();
+                    for (DocumentSnapshot document : list) {
+                        Posts p = document.toObject(Posts.class);
+                        if(Objects.isNull(p)){
+                            Toast.makeText(getContext(), "Null Object", Toast.LENGTH_SHORT).show();
+                        } else {
+                            postList.add(p);
+                        }
+
+                    }
+                    pAdapter = new PostAdapter(getContext(), postList);
+                    postListView.setAdapter(pAdapter);
+                } else {
+                    Toast.makeText(getContext(), "List Empty", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                // we are displaying a toast message
+                // when we get any error from Firebase.
+                Toast.makeText(getContext(), "Fail to load data..", Toast.LENGTH_SHORT).show();
+            }
+        });
+        return root;
     }
 }
