bd3c6ce674685c26a8eec57dc274bcef6cba7eee
jcx201495 <jcx2014@gmail.com>
2022-12-15 11:08:52 -0600

    followers work

--- app/src/main/java/com/example/fitlink1/SearchAdapter.java
+++ app/src/main/java/com/example/fitlink1/SearchAdapter.java
@@ -18,6 +18,8 @@ import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FieldValue;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.lang.reflect.Array;
@@ -57,13 +59,20 @@ public class SearchAdapter extends ArrayAdapter<UserName> {
                 String id = user.getUid();
                 String Added = p.getUsername(); // trying to get array from firestore to update friendlist
                 String[] array;
+
+                DocumentReference washingtonRef = db.collection("users").document(id);
+
+
+                washingtonRef.update("friendsList", FieldValue.arrayUnion(Added));
+
+
                 //array = db.collection("user").document(id).get("friendsList");
-                Map<String, Object> docData = new HashMap<>();
-                docData.put("FriendsList", Arrays.asList(Added));
+               // Map<String, Object> docData = new HashMap<>();
+                //docData.put("FriendsList", Arrays.asList(Added));
 
 
                 Toast.makeText(getContext(), "User Followed: " + p.getUsername(),Toast.LENGTH_SHORT).show();
-                db.collection("users").document(id).update(docData);
+                //db.collection("users").document(id).update(docData);
 
             }
         });
